7a9f3e525699f7475357ed2400a7d1333ef332d9
EC2 Default User <ec2-user@ip-172-31-10-150.us-west-2.compute.internal>
2017-12-03 19:43:39 +0000

    final app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  cognito: {
+    USER_POOL_ID: "us-west-2_sylkH5hVA",
+    APP_CLIENT_ID: "3rvfejit0lvpss34unrdqt2hjt",
+    REGION: "us-west-2",
+    IDENTITY_POOL_ID: "us-west-2:e02fc3d5-d56b-4c89-8a34-73c9bddf472e"
+  },
+  apiGateway: {
+    URL: "https://ceknafw3ng.execute-api.us-west-2.amazonaws.com/prod",
+    REGION: "us-west-2"
+  },
+  s3: {
+  BUCKET: "notes-ddpr87"
+  }
+}
